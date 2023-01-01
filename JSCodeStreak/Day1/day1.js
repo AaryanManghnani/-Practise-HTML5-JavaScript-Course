6e296ba41a977ddc4a77aa47dcc35c02dc0b947b
AaryanManghnani <108581214+AaryanManghnani@users.noreply.github.com>
2023-01-01 22:29:31 +0530

    Create day1.js

---
+++ JSCodeStreak/Day1/day1.js
@@ -0,0 +1,18 @@
+let firstName = 'Aaryan'    
+let lastName = 'Manghnani'     
+let country = 'India'      
+let city = 'Jodhpur'         
+let age = 19
+let isMarried = false
+
+const gravity = 9.81         
+const boilingPoint = 100   
+const PI = 3.14          
+
+let name = 'Aaryan',     
+  job = 'Student',
+  live = 'India'
+
+console.log(firstName, lastName, country, city, age, isMarried) 
+console.log(gravity, boilingPoint, PI) 
+console.log(name, job, live)
